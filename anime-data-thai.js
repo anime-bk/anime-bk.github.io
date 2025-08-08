34b386968501b2a40513e7cf6d7cae00b6e1868d
yyy-anime <haygontt@gmail.com>
2025-08-09 03:31:12 +0700

    Update anime-data-thai.js

--- anime-data-thai.js
+++ anime-data-thai.js
@@ -610,13 +610,6 @@ const animeList = [
     type: "ตอนที่ 1-9 พากย์ไทย",
     link: "https://anime-bk-listv2.github.io/Great-Pretender-Ss2-Thai.html"
 },
-  {
-  
-    title: "Great Pretender ยอดคนลวงโลก ภาค2 ตอนที่ 1-9 พากย์ไทย",
-    img: "https://img2.pic.in.th/pic/imaged43bc80afbfcaa88.jpg",
-    type: "ตอนที่ 1-9 พากย์ไทย",
-    link: "https://anime-bk-listv2.github.io/Great-Pretender-Ss2-Thai.html"
-},
 
 
 
