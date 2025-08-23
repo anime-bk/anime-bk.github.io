b5ebbe8ab3d7dccd9e05fb718a9570b5b7a88d22
yyy-anime <haygontt@gmail.com>
2025-08-23 18:29:14 +0700

    Add files via upload

--- anime-data.js
+++ anime-data.js
@@ -1,5 +1,14 @@
 const animeList = [
 
+
+{
+  
+    title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-161 ซับไทย",
+    img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-5-1_11zon3cf7c93b89dc03bb.jpg",
+    type: "ตอนที่ 1-161 ซับไทย.",
+    link: "https://fights-break-sphere-season-5.github.io/"
+},
+
 {
   
     title: "Wushen Zhuzai มหาศึกเทพสงคราม ตอนที่ 1-569 ซับไทย",
@@ -252,13 +261,6 @@ const animeList = [
     link: "https://mushen-ji.github.io/thai"
 },
 
-{
-  
-    title: "Fights Break Sphere Season 5 สัประยุทธ์ทะลุฟ้า ภาค 5 ตอนที่ 1-161 ซับไทย",
-    img: "https://img2.pic.in.th/pic/Fights-Break-Sphere-Season-5-1_11zon3cf7c93b89dc03bb.jpg",
-    type: "ตอนที่ 1-161 ซับไทย.",
-    link: "https://fights-break-sphere-season-5.github.io/"
-},
 
 {
   
