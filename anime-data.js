f0569de81ec7731bc7af9f1779451633ebbac3aa
yyy-anime <haygontt@gmail.com>
2025-10-05 12:25:29 +0700

    Update anime-data.js

--- anime-data.js
+++ anime-data.js
@@ -2,9 +2,9 @@ const animeList = [
 
   {
   
-    title: "Mushen Ji – ตำนานเทพกู้จักรวาล ตอนที่ 1-50 ซับไทย",
+    title: "Mushen Ji – ตำนานเทพกู้จักรวาล ตอนที่ 1-51 ซับไทย",
     img: "https://img5.pic.in.th/file/secure-sv1/Mushen-Ji.jpg",
-    type: "ตอนที่ 1-50 ซับไทย.",
+    type: "ตอนที่ 1-51 ซับไทย.",
     link: "https://mushen-ji.github.io/"
 },
 
